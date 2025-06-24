dd4461ff35b49c8beaf85cd499202581bb0145aa
BenitoFezay <benitofezay@gmail.com>
2025-06-24 12:10:14 +0300

    Ajout de nouvelle route.

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -5,12 +5,14 @@ import { Navigate, useRoutes } from "react-router-dom";
 import UtilsProtectAuthRoutes from "../utils/routes/UtilsProtectAuthRoutes";
 
 // V I E W S
-import AuthIndexRegister from "../views/auth/pages/AuthIndexRegister";
 import AuthLayout from "../views/auth/layout/AuthLayout";
 import AuthIndexLogin from "../views/auth/pages/AuthIndexLogin";
-import ClientIndexHomePage from "../views/client/pages/home/ClientIndexHomePage";
+import AuthIndexRegister from "../views/auth/pages/AuthIndexRegister";
 import ClientLayout from "../views/client/layout/ClientLayout";
+import ClientIndexAbout from "../views/client/pages/about/ClientIndexAbout";
 import ClientIndexCircuit from "../views/client/pages/circuits/ClientIndexCircuit";
+import IndexCircuitDetails from "../views/client/pages/circuits/IndexCircuitDetails";
+import ClientIndexHomePage from "../views/client/pages/home/ClientIndexHomePage";
 
 // MAIN FUNCTION: ROUTES
 export default function Routes() {
@@ -24,7 +26,9 @@ export default function Routes() {
       element: <ClientLayout />,
       children: [
         { path: "index", element: <ClientIndexHomePage />, index: true }, // Home page.
-        { path: "circuits", element: <ClientIndexCircuit /> }, // Home page.
+        { path: "about", element: <ClientIndexAbout /> }, // about page.
+        { path: "circuits", element: <ClientIndexCircuit /> }, // circuits page.
+        { path: "circuits/:slug/:id", element: <IndexCircuitDetails /> }, // circuits-details page.
       ],
     },
 
