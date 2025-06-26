5cf8775e39dd80c558a403dfc71474fa6a074af7
BenitoFezay <benitofezay@gmail.com>
2025-06-26 13:18:27 +0300

    Modification de best circuits.

--- src/views/client/pages/home/ClientIndexHomePage.jsx
+++ src/views/client/pages/home/ClientIndexHomePage.jsx
@@ -1,18 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import MakiMada from "../../../../assets/images/03.jpg";
-import BaobabSunSet from "../../../../assets/images/baobab5.jpg";
-import Tsingy from "../../../../assets/images/tsingy.jpg";
-import Lezard from "../../../../assets/images/05.jpg";
+import Isalo from "../../../../assets/images/Isalo.jpg";
+import NosyIranja from "../../../../assets/images/Madagascar.jpg";
+import Maki from "../../../../assets/images/maki.jpg";
 
-import ClientHomeSectionBanner from "./ClientHomeSectionBanner";
-import ClientHomeHeroSection from "./ClientHomeHeroSection";
-import ClientHomeSearchSection from "./search/ClientHomeSearchSection";
 import ClientHomeCircuitSection from "./ClientHomeCircuitSection";
 import ClientHomePresentationSection from "./ClientHomePresentationSection";
+import ClientHomeSearchSection from "./search/ClientHomeSearchSection";
 import HomeHeroSection from "./section/HomeHeroSection";
 import ParallaxSection from "./section/ParallaxSection";
+import { TbWalk } from "react-icons/tb";
+import { BsCurrencyEuro, BsFillSunFill } from "react-icons/bs";
+
+const circuits = [
+  {
+    id: 1,
+    slug: "au-fond-de-la-mer",
+    price: 1080,
+    image: NosyIranja,
+    name: "Au fond de la mer de Salary Tuléar",
+    theme: "Découverte - Tour Bleu",
+    difficult: "Moyenne",
+    days: 12,
+    night: 11,
+  },
+  {
+    id: 2,
+    slug: "la-rn7-au-sud",
+    price: 990,
+    image: Isalo,
+    name: "La RN7 et Isalo Madagascar",
+    theme: "Aventure - Trekking",
+    difficult: "Difficle",
+    days: 8,
+    night: 7,
+  },
+  {
+    id: 3,
+    slug: "rencontrer-la-foret-tropicale",
+    price: 840,
+    image: Maki,
+    name: "Rencontrer la forêt tropicale de Ranomafana et ses faunes",
+    theme: "Découverte - Nature",
+    difficult: "Facile",
+    days: 6,
+    night: 5,
+  },
+];
 
 export default function ClientIndexHomePage() {
   return (
@@ -43,312 +78,99 @@ export default function ClientIndexHomePage() {
       {/* END BANER SECTION */}
 
       <div className="mx-auto max-w-screen-2xl mt-7 px-4 lg:px-7">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-4">
-          <div className="rounded">
-            <Link
-              to="#"
-              className="block rounded-lg p-4 shadow-xs shadow-indigo-100"
-            >
-              <img
-                alt=""
-                src={BaobabSunSet}
-                className="h-56 w-full rounded-xl object-cover shadow-xl"
-              />
+        <div className="grid grid-cols-1 gap-7 lg:gap-x-10 lg:gap-y-12 lg:grid-cols-3">
+          {circuits.map((circuit, index) => (
+            <div key={index} className="rounded">
+              <Link
+                to={`/circuits/${circuit?.slug}/${circuit?.id}`}
+                className="block rounded-lg shadow-xs shadow-indigo-100 group"
+              >
+                <img
+                  alt=""
+                  src={circuit?.image}
+                  className="h-56 w-full rounded-xl object-cover shadow-xl"
+                />
 
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
+                <div className="relative -mt-11 text-center w-2/5">
+                  <p className="text-xs lg:text-sm font-bold tracking-widest rounded-r-xl bg-slate-950/65 py-2 text-slate-50 flex items-center justify-center">
+                    À partir de &nbsp;
+                    <BsCurrencyEuro />
+                    {circuit?.price}
+                  </p>
+                </div>
+                <div className="mt-2 px-1">
+                  <dl>
+                    <div>
+                      <dt className="sr-only">theme</dt>
 
-                    <dd className="text-sm text-gray-500">À partir de $440</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="text-slate-800 font-semibold">
-                      123 Wallaby Avenue, Park Road
-                    </dd>
-                  </div>
-                </dl>
-
-                <div className="mt-6 flex items-center gap-8 text-xs">
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Parking</p>
-
-                      <p className="font-semibold text-slate-800">2 spaces</p>
+                      <dd className="text-xs font-medium text-slate-700">
+                        {circuit?.theme}
+                      </dd>
                     </div>
-                  </div>
 
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                      />
-                    </svg>
+                    <div>
+                      <dt className="sr-only">Name</dt>
 
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bathroom</p>
-
-                      <p className="font-semibold text-slate-800">2 rooms</p>
+                      <dd className="text-slate-800 group-hover:underline text-sm line-clamp-1 font-semibold">
+                        {circuit?.name}
+                      </dd>
                     </div>
-                  </div>
+                  </dl>
 
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                      />
-                    </svg>
+                  <div className="mt-6 flex items-center gap-8 text-xs">
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <TbWalk className="size-4 text-rose-600" />
 
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bedroom</p>
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-slate-700">Difficulté</p>
 
-                      <p className="font-semibold text-slate-800">4 rooms</p>
+                        <p className="font-semibold text-slate-800 lowercase">
+                          {circuit?.difficult}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <BsFillSunFill className="size-4 text-yellow-400" />
+
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-slate-700">Séjours</p>
+
+                        <p className="font-semibold text-slate-800">
+                          {circuit?.days} jours
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                      <svg
+                        className="size-4 text-rose-600"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                        />
+                      </svg>
+
+                      <div className="mt-1.5 sm:mt-0">
+                        <p className="text-slate-700">Nuités</p>
+
+                        <p className="font-semibold text-slate-800">
+                          {circuit?.night} nuits
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="rounded">
-            <Link
-              to="#"
-              className="block rounded-lg p-4 shadow-xs shadow-indigo-100"
-            >
-              <img
-                alt=""
-                src={Lezard}
-                className="h-56 w-full rounded-xl object-cover shadow-xl"
-              />
-
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
-
-                    <dd className="text-sm text-gray-500">À partir de $440</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="text-slate-800 font-semibold">
-                      123 Wallaby Avenue, Park Road
-                    </dd>
-                  </div>
-                </dl>
-
-                <div className="mt-6 flex items-center gap-8 text-xs">
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Parking</p>
-
-                      <p className="font-semibold text-slate-800">2 spaces</p>
-                    </div>
-                  </div>
-
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bathroom</p>
-
-                      <p className="font-semibold text-slate-800">2 rooms</p>
-                    </div>
-                  </div>
-
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bedroom</p>
-
-                      <p className="font-semibold text-slate-800">4 rooms</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div>
-
-          <div className="rounded">
-            <Link
-              to="#"
-              className="block rounded-lg p-4 shadow-xs shadow-indigo-100"
-            >
-              <img
-                alt=""
-                src={Tsingy}
-                className="h-56 w-full rounded-xl object-cover shadow-xl"
-              />
-
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
-
-                    <dd className="text-sm text-gray-500">À partir de $440</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="text-slate-800 font-semibold">
-                      123 Wallaby Avenue, Park Road
-                    </dd>
-                  </div>
-                </dl>
-
-                <div className="mt-6 flex items-center gap-8 text-xs">
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Parking</p>
-
-                      <p className="font-semibold text-slate-800">2 spaces</p>
-                    </div>
-                  </div>
-
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bathroom</p>
-
-                      <p className="font-semibold text-slate-800">2 rooms</p>
-                    </div>
-                  </div>
-
-                  <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                    <svg
-                      className="size-4 text-rose-600"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                      />
-                    </svg>
-
-                    <div className="mt-1.5 sm:mt-0">
-                      <p className="text-gray-500">Bedroom</p>
-
-                      <p className="font-semibold text-slate-800">4 rooms</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
 
