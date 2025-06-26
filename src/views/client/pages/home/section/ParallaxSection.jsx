e94c986c4fc5b75088ef0a6b47a0cb9cd8392f3f
BenitoFezay <benitofezay@gmail.com>
2025-06-26 12:08:04 +0300

    Ajout de Testimonial customer.

--- src/views/client/pages/home/section/ParallaxSection.jsx
+++ src/views/client/pages/home/section/ParallaxSection.jsx
@@ -13,15 +13,38 @@ const ParallaxSection = () => {
       >
         <div className="w-full h-full bg-black/50">
           <div className="mx-auto mt-24 text-center max-w-2xl">
-            <h2 className="text-4xl font-bold text-slate-50 lg:text-5xl">
-              Understand flow
-              <strong className="text-rose-600"> increase </strong>
-            </h2>
-
-            <p className="mt-4 text-base font-semibold text-pretty text-slate-300 sm:text-3xl">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
-              iusto.
-            </p>
+            <div className="w-full flex justify-center items-center">
+              <figure className="max-w-screen-md mx-auto">
+                <svg
+                  className="h-12 mx-auto mb-3 text-rose-600"
+                  viewBox="0 0 24 27"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+                <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">
+                  "Landwind is just awesome. It contains tons of predesigned
+                  components and pages starting from login screen to complex
+                  dashboard. Perfect choice for your next SaaS application."
+                </p>
+                <figcaption className="flex items-center justify-center mt-6 space-x-3">
+                  <img
+                    className="w-7 h-7 rounded-full"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+                    alt="Michael Gough"
+                  />
+                  <div className="flex items-center divide-x-2 divide-slate-50">
+                    <div className="pr-3 font-medium text-white">
+                      <h4>Micheal Gough</h4>
+                    </div>
+                  </div>
+                </figcaption>
+              </figure>
+            </div>
           </div>
         </div>
       </section>
